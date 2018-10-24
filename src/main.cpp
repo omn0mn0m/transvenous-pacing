334fab9007b16cd7364e861cc1be8849b19580ae
Nam Tran <tranngocnam97@gmail.com>
2018-10-24 07:50:00 -0400

    Functions more!

--- src/main.cpp
+++ src/main.cpp
@@ -12,40 +12,28 @@ Serial pc_serial(PC_SERIAL_TX, PC_SERIAL_RX);
 // Global Variables
 u_int16_t samples[SAMPLE_SIZE];
 int sum_samples = 0;
-u_int16_t avg_reading;
+u_int16_t ir_avg_0;
 u_int16_t ambient_lvl;
+
+// Global Functions
+void ir_init(AnalogIn ir_sensor);
+u_int16_t get_moving_average();
 
 /**
  *  Main code for the microcontroller
  */
-int main() {
+int 
+main(void) {
     // put your setup code here, to run once:
-    pc_serial.printf("Phantom Initialised\n");
+    ir_init(ir_0);
 
-    for (int i = 0; i < SAMPLE_SIZE; i++) {
-        samples[i] = ir_0.read_u16();
-        sum_samples += samples[i];
-    }
-
-    avg_reading = sum_samples / SAMPLE_SIZE;
-    ambient_lvl = avg_reading;
-
-    pc_serial.printf("%d\n", avg_reading);
+    pc_serial.printf("Initialised\n");
 
     while (1) {
         // put your main code here, to run repeatedly:
-        int old_sample = samples[0];
+        ir_avg_0 = get_moving_average();
 
-        for (int i = 1; i < SAMPLE_SIZE; i++) {
-            samples[i - 1] = samples[i];
-        }
-
-        samples[SAMPLE_SIZE - 1] = ir_0.read_u16();
-
-        sum_samples = sum_samples + samples[SAMPLE_SIZE - 1] - old_sample;
-        avg_reading = sum_samples / SAMPLE_SIZE;
-
-        if (abs(ambient_lvl - avg_reading) >= THRESHOLD) {
+        if (abs(ambient_lvl - ir_avg_0) >= THRESHOLD) {
             pc_serial.putc('0');
         } 
         // else if (ir_1 <= THRESHOLD) {
@@ -54,4 +42,32 @@ int main() {
 
         wait_ms(10);
     }
+}
+
+void 
+ir_init(AnalogIn ir_sensor) {
+    for (int i = 0; i < SAMPLE_SIZE; i++) {
+        samples[i] = ir_sensor.read_u16();
+        sum_samples += samples[i];
+    }
+
+    ir_avg_0 = sum_samples / SAMPLE_SIZE;
+    ambient_lvl = ir_avg_0;
+
+    // pc_serial.printf("%d\n", avg_reading);
+}
+
+u_int16_t
+get_moving_average() {
+    int old_sample = samples[0];
+
+    for (int i = 1; i < SAMPLE_SIZE; i++) {
+        samples[i - 1] = samples[i];
+    }
+
+    samples[SAMPLE_SIZE - 1] = ir_0.read_u16();
+
+    sum_samples = sum_samples + samples[SAMPLE_SIZE - 1] - old_sample;
+
+    return sum_samples / SAMPLE_SIZE;
 }
