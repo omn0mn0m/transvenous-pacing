be9b9e5daefbb69f1386065103c4537560cb5709
Nam Tran <tranngocnam97@gmail.com>
2018-10-23 20:02:45 -0400

    Edited for next step of project

---
+++ src/config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+// Software Constants
+#define THRESHOLD 500
+
+// Hardware Constants
+#define PORT_IR_0 A0
+#define PORT_IR_1 A1
+#define PORT_IR_2 A2
+
+#define PC_SERIAL_TX USBTX
+#define PC_SERIAL_RX USBRX
+
+// Variable Constants
+#define SAMPLE_SIZE 32
+#define AMBIENT_SIZE 256
+
+#endif // CONFIG_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,10 @@
 #include <mbed.h>
-
-// Software Constants
-#define THRESHOLD 300
-
-// Hardware Constants
-#define PORT_IR_0 A0
-#define PORT_IR_1 A1
-
-#define PC_SERIAL_TX USBTX
-#define PC_SERIAL_RX USBRX
-
-// Variable Constants
-#define SAMPLE_SIZE 32
+#include "config.h"
 
 // Global Pin Assignments
 AnalogIn ir_0(PORT_IR_0);
 AnalogIn ir_1(PORT_IR_1);
+AnalogIn ir_2(PORT_IR_2);
 
 // Global Class Assignments
 Serial pc_serial(PC_SERIAL_TX, PC_SERIAL_RX);
@@ -42,16 +31,8 @@ int main() {
 
     pc_serial.printf("%d\n", avg_reading);
 
-    int counter = 3000;
-
     while (1) {
         // put your main code here, to run repeatedly:
-        // if (ir_0 <= THRESHOLD) {
-        //     pc_serial.putc('0');
-        // } else if (ir_1 <= THRESHOLD) {
-        //     pc_serial.putc('1');
-        // }
-
         int old_sample = samples[0];
 
         for (int i = 1; i < SAMPLE_SIZE; i++) {
@@ -63,11 +44,12 @@ int main() {
         sum_samples = sum_samples + samples[SAMPLE_SIZE - 1] - old_sample;
         avg_reading = sum_samples / SAMPLE_SIZE;
 
-        if (counter == 0) {
-            pc_serial.printf("%d\n", avg_reading);
-        } else {
-            counter--;
-        }
+        if (avg_reading <= THRESHOLD) {
+            pc_serial.putc('0');
+        } 
+        // else if (ir_1 <= THRESHOLD) {
+        //     pc_serial.putc('1');
+        // }
 
         wait_ms(10);
     }
