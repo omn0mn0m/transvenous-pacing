8d36663344b11567f38c0056c40ce48c36f4eadc
Nam Tran <tranngocnam97@gmail.com>
2018-11-07 16:35:13 -0500

    Moved headers to include dir

---
+++ include/config.h
@@ -0,0 +1,24 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+// Software Constants
+#define THRESHOLD 1000
+
+// Hardware Constants
+#define PORT_IR_0 A0
+#define PORT_IR_1 A1
+#define PORT_IR_2 A2
+#define PORT_IR_3 A3
+#define PORT_IR_4 A4
+#define PORT_IR_5 A5
+#define PORT_IR_6 A6
+#define PORT_IR_7 D3
+#define PORT_IR_8 D6
+
+#define PC_SERIAL_TX USBTX
+#define PC_SERIAL_RX USBRX
+
+// Variable Constants
+#define SAMPLE_SIZE 8
+
+#endif // CONFIG_H_
