33ca75c15ebb64d979013b8d2826cfec0ae2a7e1
Nam Tran <tranngocnam97@gmail.com>
2019-03-31 18:12:08 -0400

    Pins moved to avoid DAC pin (for BT)

--- include/config.h
+++ include/config.h
@@ -15,12 +15,12 @@
 // #define PORT_IR_7 26
 // #define PORT_IR_8 27
 
-#define PORT_IR_0 39
-#define PORT_IR_1 34
-#define PORT_IR_2 35
-#define PORT_IR_3 32
-#define PORT_IR_4 33
-#define PORT_IR_5 25
+#define PORT_IR_0 36
+#define PORT_IR_1 39
+#define PORT_IR_2 34
+#define PORT_IR_3 35
+#define PORT_IR_4 32
+#define PORT_IR_5 33
 
 #define PC_SERIAL_TX USBTX
 #define PC_SERIAL_RX USBRX
